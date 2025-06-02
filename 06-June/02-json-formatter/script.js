bde767f10bab02170d09bc607b36635d042d1ac5
Glory42 <gorkemkaryol03@gmail.com>
2025-06-02 17:46:08 +0300

    chore: added comments to js file.

--- 06-June/02-json-formatter/script.js
+++ 06-June/02-json-formatter/script.js
@@ -1,4 +1,5 @@
 function formatJSON() {
+    // assiging html ids to js variables bla bla.
     const input = document.getElementById('json-entry');
     const resultEl = document.getElementById('result');
     const errorEl  = document.getElementById('error');
@@ -6,6 +7,7 @@ function formatJSON() {
     resultEl.textContent = '';
     errorEl.textContent = '';
 
+    // Using JSON '.parse' and '.stringify' first getting JSON entry than formatting.
     try {
         const parsed = JSON.parse(input.value); 
         const formatted = JSON.stringify(parsed, null, 2);
