1a3e16b03a23506d80e318ee9c6dc99153291a18
Glory42 <gorkemkaryol03@gmail.com>
2025-06-05 23:28:47 +0300

    feat: made fetching user data and repos using github-api.

---
+++ 06-June/03-github-profile-analyzer/scripts/github-api.js
@@ -0,0 +1,17 @@
+const BASE_URL = 'https://api.github.com';
+
+export async function getUserData(username) {
+    const response = await fetch(`${BASE_URL}/${username}`);
+
+    if (!response.ok) throw new Error('User not found');
+
+    return response.json();
+}
+
+export async function getUserRepos(username) {
+    const response = await fetch(`${BASE_URL}/${username}/repos?per_page=100`);
+
+    if (!response.ok) throw new Error('User repos not found');
+
+    return response.json();
+}
