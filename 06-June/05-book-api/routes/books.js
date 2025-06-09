61c53c027f040f1d4d61f4ca5f8b34e43fe47653
Glory42 <gorkemkaryol03@gmail.com>
2025-06-10 00:11:38 +0300

    feat: made route for crud actions.

---
+++ 06-June/05-book-api/routes/books.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const {
+  createBook,
+  getBooks,
+  updateBook,
+  deleteBook,
+} = require('../controllers/booksController');
+
+router.post('/', createBook);
+router.get('/', getBooks);
+router.put('/:id', updateBook);
+router.delete('/:id', deleteBook);
+
+module.exports = router;
